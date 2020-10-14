7766d48714a9c14a105c78bfbbb1fae8cc0324a8
AlexMykhailov1 <anonim2771@gmail.com>
2020-10-14 12:37:59 +0300

    C1

--- Lab 2.2 report.cpp
+++ Lab 2.2 report.cpp
@@ -12,7 +12,7 @@ using namespace std;
 int main()
 {
 	double a;
-	double z1;
+	//double z1;
 	double z2;
 
 	cout << "a = "; cin >> a;
@@ -22,11 +22,11 @@ int main()
 		cout << "a = "; cin >> a;
 	}
 
-	z1 = pow((1 + a + a * a) / (2 * a + a * a) + 2 - (1 - a + a * a) / (2 * a - a * a), -1) * (5 - 2 * a * a);
+	//z1 = pow((1 + a + a * a) / (2 * a + a * a) + 2 - (1 - a + a * a) / (2 * a - a * a), -1) * (5 - 2 * a * a);
 	z2 = (4 - a * a) / 2;
 
 	cout << endl;
-	cout << "z1 = " << z1 << endl;
+	//cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
 
 	cin.get();
